d9f85f6f638607f812db9aad9c2c26b785454bb0
JorginhoO <jorge.infoo88@gmail.com>
2023-12-14 13:05:51 -0300

    Finalized server structure

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -10,7 +10,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const runServer = (port: number, server: http.Server) => {
+    server.listen(port, () => {
+        console.log(`🚀 Running at Port ${port}`);
+    });
+}
 
+const regularServer = http.createServer(app);
+if (process.env.NODE_ENV === 'production') {
+    // TODO: configurar SSL
+    // TODO: rodar server na 80 e na 443
+} else {
+    const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000;
+    runServer(serverPort, regularServer);
+}
 
 
 
